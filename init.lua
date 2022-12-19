21bc7a124f457344d9b57c2aa2994d61d4e0417d
jacksonneal <jax@jneal-pc.localdomain>
2022-12-19 18:16:27 -0500

    impatient

--- init.lua
+++ init.lua
@@ -14,4 +14,5 @@ require("user.nvim-tree")
 require("user.lualine")
 require("user.toggleterm")
 require("user.project")
+require("user.impatient")
 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,6 +54,7 @@ return packer.startup(function(use)
     use "nvim-lualine/lualine.nvim"
     use "akinsho/toggleterm.nvim"
     use "ahmedkhalf/project.nvim"
+    use "lewis6991/impatient.nvim"
 
     -- Color schemes
     use "folke/tokyonight.nvim" 
