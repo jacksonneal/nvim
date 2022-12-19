ac6662d2d95e6b7803a867184e58469cd315184d
jacksonneal <jax@jneal-pc.localdomain>
2022-12-19 14:03:50 -0500

    gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -80,6 +80,9 @@ return packer.startup(function(use)
     use "nvim-treesitter/playground" 
     use "JoosepAlviste/nvim-ts-context-commentstring"
 
+    -- Git 
+    use "lewis6991/gitsigns.nvim"
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
