d801a63b8e6365e39bf49e2e1ea2ae02c85935fc
jacksonneal <jax@jneal-pc.localdomain>
2022-12-22 18:18:08 -0500

    whichkey working

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -58,7 +58,7 @@ return packer.startup(function(use)
 	use("goolord/alpha-nvim")
 	use("antoinemadec/FixCursorHold.nvim")
 	use("folke/which-key.nvim")
-
+ 
 	-- Color schemes
 	use("folke/tokyonight.nvim")
 
