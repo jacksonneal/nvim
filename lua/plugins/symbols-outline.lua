7ab945760fa4a3f0b56a89931f589863d6434bf0
jacksonneal <jacksontneal@gmail.com>
2023-02-04 13:27:34 -0500

    plugin work

---
+++ lua/plugins/symbols-outline.lua
@@ -0,0 +1,20 @@
+return {
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
