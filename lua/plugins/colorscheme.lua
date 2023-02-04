6bf4bf5dab5031447905616ec55b3bba598703cd
jacksonneal <jacksontneal@gmail.com>
2023-02-04 15:03:57 -0500

    tree and window picker

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      require("rose-pine").setup()
+      require("rose-pine").setup({
+        dark_variant = "moon",
+      })
       vim.cmd("colorscheme rose-pine")
     end,
   },
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "s1n7ax/nvim-window-picker",
+    tag = "v1.*",
+    config = function()
+      require("window-picker").setup({
+        -- the foreground (text) color of the picker
+        fg_color = "#2a273f",
+        -- if you have include_current_win == true, then current_win_hl_color will
+        -- be highlighted using this background color
+        current_win_hl_color = "#ea9a97",
+        -- all the windows except the curren window will be highlighted using this
+        -- color
+        other_win_hl_color = "#ea9a97",
+      })
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            ["h"] = "close_node",
+            ["l"] = "open",
+          },
+        },
+      },
+    },
+  },
+}
