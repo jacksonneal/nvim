f28bb202672e81a54a13586aea5a7d6329baa6a0
jacksonneal <jacksontneal@gmail.com>
2023-02-04 01:20:57 -0500

    init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
