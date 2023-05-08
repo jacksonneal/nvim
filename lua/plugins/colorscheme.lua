fd38daf0a8a52b627cf990b76d974b5e380a72fb
Jackson Neal <jackson.neal@pathai.com>
2023-05-08 10:17:15 -0400

    style

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,12 +4,9 @@ return {
     name = "rose-pine",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("rose-pine").setup({
-        dark_variant = "moon",
-      })
-      vim.cmd("colorscheme rose-pine")
-    end,
+    config = {
+      dark_variant = "moon",
+    },
   },
   {
     "LazyVim/LazyVim",
