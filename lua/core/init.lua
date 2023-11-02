cc0775e396c85899849a839dceeb02c7e1d1d345
jacksonneal <jacksontneal@gmail.com>
2023-11-02 13:41:12 -0400

    add some custom styling

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -48,6 +48,17 @@ require("lazy").setup({
     lazy = false,
     priority = 1000,
     config = function()
+      vim.g.gruvbox_material_better_performance = 1
+      -- Fonts
+      vim.g.gruvbox_material_disable_italic_comment = 1
+      vim.g.gruvbox_material_enable_italic = 0
+      vim.g.gruvbox_material_enable_bold = 0
+      -- vim.g.gruvbox_material_transparent_background = 1
+      -- Themes
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
       -- set colorscheme
       vim.cmd.colorscheme(settings.colorscheme)
     end,
