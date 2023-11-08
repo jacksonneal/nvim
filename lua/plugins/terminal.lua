cfb76e816299c54cbd796bbc334fd759b230ad23
jacksonneal <jacksontneal@gmail.com>
2023-11-08 01:31:27 -0500

    add terminal

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,13 @@
+-- Module for terminal plugins.
+
+local plugins = {
+  {
+    "akinsho/toggleterm.nvim",
+    config = {
+      open_mapping = [[<c-\>]],
+      size = 18,
+    },
+  },
+}
+
+return plugins
