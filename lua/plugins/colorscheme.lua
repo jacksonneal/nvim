3678c41e57ba51ac1d9ca3369a39c248235dd243
jacksonneal <jacksontneal@gmail.com>
2024-04-08 23:20:50 -0400

    update gruvbox-material

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,35 +1,27 @@
--- Module for colorscheme plugins.
+-- Colorscheme plugins.
 
 local settings = require("core.config").settings
 
-local function set_colorscheme()
-  vim.cmd.colorscheme(settings.colorscheme)
-end
-
-local function gruvbox_material_config()
-  vim.g.gruvbox_material_better_performance = 1
-
-  vim.g.gruvbox_material_disable_italic_comment = 1
-  vim.g.gruvbox_material_enable_italic = 0
-  vim.g.gruvbox_material_enable_bold = 0
-
-  vim.g.gruvbox_material_foreground = "mix"
+-- Configure gruvbox-material colorscheme.
+local function config_gruvbox_material()
   vim.g.gruvbox_material_background = "hard"
-  vim.g.gruvbox_material_ui_contrast = "high"
+  vim.g.gruvbox_material_better_performance = 1
+  vim.g.gruvbox_material_disable_italic_comment = 1
   vim.g.gruvbox_material_float_style = "dim"
-
-  set_colorscheme()
+  vim.g.gruvbox_material_foreground = "mix"
+  vim.g.gruvbox_material_ui_contrast = "high"
 end
 
-local plugins = {
+return {
   {
     -- gruvbox with softened palette
     "sainnhe/gruvbox-material",
     enabled = settings.colorscheme == "gruvbox-material",
     lazy = false,
     priority = 1000,
-    config = gruvbox_material_config,
+    config = function()
+      config_gruvbox_material()
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
   },
 }
-
-return plugins
