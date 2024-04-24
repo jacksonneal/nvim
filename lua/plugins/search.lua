aad4396045e199c5b4d20aeee975b46cf134493e
Jackson Neal <jackson.neal@pathai.com>
2024-04-24 14:23:31 -0400

    use ripgrep for file picker

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -6,7 +6,7 @@ local plugins = {
     "echasnovski/mini.pick",
     lazy = false,
     keys = {
-      { "<leader><leader>", "<cmd>Pick files tool='git'<cr>", desc = "Search files" },
+      { "<leader><leader>", "<cmd>Pick files tool='rg'<cr>", desc = "Search files" },
       { "<leader>/", "<cmd>Pick grep tool='rg'<cr>", desc = "Search global" },
       { "<leader>R", "<cmd>Pick resume<cr>", desc = "Resume search" },
     },
